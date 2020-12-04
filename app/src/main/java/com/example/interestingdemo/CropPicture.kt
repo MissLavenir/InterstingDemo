79f0b5ad824a50aac1c732823961d34664d58f49
rengongzheng <1259940673@qq.com>
2020-12-04 17:06:01 +0800

    修改图片裁剪bug

--- app/src/main/java/com/example/interestingdemo/CropPicture.kt
+++ app/src/main/java/com/example/interestingdemo/CropPicture.kt
@@ -1,26 +1,27 @@
 package com.example.interestingdemo
 
+import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
-import android.content.ContentResolver
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
-import androidx.core.net.toFile
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.Fragment
+import com.example.interestingdemo.extensions.isAlive
 import com.example.interestingdemo.extensions.toast
 import com.yalantis.ucrop.UCrop
 import kotlinx.android.synthetic.main.dialog_sure_btn.view.*
 import kotlinx.android.synthetic.main.fragment_crop_picture.*
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
+import java.io.File
 
 
 @Suppress("DEPRECATION")
@@ -32,6 +33,15 @@ class CropPicture : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_crop_picture, container, false)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this)
     }
 
     @RequiresApi(Build.VERSION_CODES.KITKAT)
@@ -49,50 +59,81 @@ class CropPicture : Fragment() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK){
-            when(requestCode){
-                REQUEST_IMAGE -> {
-                    data?.data?.let { uri ->
-                        val dialog = LayoutInflater.from(context).inflate(R.layout.dialog_sure_btn,null,false)
-                        val alert = AlertDialog.Builder(context).setView(dialog).create()
-                        dialog.sureTitle.text = "提示："
-                        dialog.sureMessage.text = "是否裁剪图片"
-                        dialog.sureBtn.setOnClickListener {
-                            cropRawPicture(uri)
-                            alert.dismiss()
-                        }
-                        dialog.cancelBtn.setOnClickListener {
-                            getPicture.setImageURI(uri)
-                            alert.dismiss()
-                        }
-                        alert.show()
+        when (resultCode) {
+            Activity.RESULT_OK -> {
+                when(requestCode){
+                    REQUEST_IMAGE -> {
+                        data?.data?.let { uri ->
+                            val dialog = LayoutInflater.from(context).inflate(R.layout.dialog_sure_btn,null,false)
+                            val alert = AlertDialog.Builder(context).setView(dialog).create()
+                            dialog.sureTitle.text = "提示："
+                            dialog.sureMessage.text = "是否裁剪图片"
+                            dialog.sureBtn.setOnClickListener {
+                                startUCropPicture(uri)
+                                alert.dismiss()
+                            }
+                            dialog.cancelBtn.setOnClickListener {
+                                getPicture.setImageURI(uri)
+                                alert.dismiss()
+                            }
+                            alert.show()
 
+                        }
                     }
-                }
-                UCrop.REQUEST_CROP -> {
-                    if (data != null){
-                        UCrop.getOutput(data)?.let { uri ->
-                            getPicture.setImageURI(uri)
+                    UCrop.REQUEST_CROP -> {
+                        if (data != null){
+                            UCrop.getOutput(data)?.let { uri ->
+                                getPicture.setImageURI(uri)
+                            }
                         }
                     }
                 }
             }
+            UCrop.RESULT_ERROR -> {
+                toast("图片裁剪失败,请重试！")
+            }
+            else -> {
+                toast("未知错误")
+            }
+        }
+    }
+
+    @AfterPermissionGranted(FILE_WRITE_PERMISSION)
+    private fun startUCropPicture(uri: Uri){
+        if (!isAlive()) return
+        val perms = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (EasyPermissions.hasPermissions(requireContext(),*perms)){
+            cropRawPicture(uri)
         }else{
-            toast("图片裁剪失败,请重试！")
+            EasyPermissions.requestPermissions(this,"需要文件读写权限才能裁剪", FILE_WRITE_PERMISSION,*perms)
         }
     }
 
 
     private fun cropRawPicture(uri : Uri){
         val options = UCrop.Options()
+        val destinationUri = Uri.fromFile(File(context?.cacheDir,"uCrop.jpg"))
         options.setCompressionFormat(Bitmap.CompressFormat.JPEG)//输出格式
         options.setMaxScaleMultiplier(2f)//最大缩放比
-        options.setCropGridColor(Color.RED)//剪辑框内线条颜色
-        options.setCropGridColumnCount(2)//剪辑框内竖线数量
-        options.setCropGridRowCount(2)//剪辑框内横线数量
-        options.setFreeStyleCropEnabled(true)//键即框宽高比可变
+        options.setFreeStyleCropEnabled(true)//框宽高比可变,默认为false,建议为true
+        options.setToolbarTitle("正在进行裁剪")//标题栏的文字
+        options.setHideBottomControls(false)//隐藏下方控制栏，默认false
+        options.setCircleDimmedLayer(false)//圆形裁剪框，默认false
+        options.setToolbarColor(ResourcesCompat.getColor(resources,R.color.black,context?.theme))//标题栏颜色
+        options.setToolbarWidgetColor(ResourcesCompat.getColor(resources,R.color.white,context?.theme))//标题和按钮颜色
+        options.setStatusBarColor(ResourcesCompat.getColor(resources,R.color.black,context?.theme))//最上方状态栏颜色
+        options.setCropGridColumnCount(2)//剪辑框内纵向网格线数量
+        options.setCropGridRowCount(2)//剪辑框内横向网格线数量
+        options.setCropGridStrokeWidth(1)//剪辑框网格线宽度
+        options.setCropGridColor(ResourcesCompat.getColor(resources,R.color.white,context?.theme))//剪辑框网格线颜色
+        options.setCropFrameStrokeWidth(2)//剪辑框宽度
+        options.setCropFrameColor(ResourcesCompat.getColor(resources,R.color.red_a200,context?.theme))//裁剪框颜色
+//        options.setToolbarCropDrawable(R.drawable.up_down_move)//设置确定图标
+//        options.setToolbarCancelDrawable(R.drawable.up_down_move)//设置返回图标
+//        options.setCropGridCornerColor(ResourcesCompat.getColor(resources,R.color.grey_500,context?.theme))//剪辑框四角颜色
+//        options.setDimmedLayerColor(ResourcesCompat.getColor(resources,R.color.grey_300,context?.theme))//裁剪框外的颜色
 
-        UCrop.of(uri,uri)
+        UCrop.of(uri,destinationUri)
             .withAspectRatio(1f,1f)
             .withMaxResultSize(500,500)
             .withOptions(options)
@@ -101,6 +142,8 @@ class CropPicture : Fragment() {
 
     companion object {
         const val REQUEST_IMAGE = 1528
+
+        const val FILE_WRITE_PERMISSION = 1
 
     }
 }
