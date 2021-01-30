d40314c7f3e5fd02dabb97cd7e23554dcc68b149
rengongzheng <1259940673@qq.com>
2021-01-30 10:52:59 +0800

    计算题生成追加

--- app/src/main/java/com/example/interestingdemo/CalculateFragment.kt
+++ app/src/main/java/com/example/interestingdemo/CalculateFragment.kt
@@ -10,7 +10,9 @@ import androidx.core.view.size
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.example.interestingdemo.extensions.finish
+import com.example.interestingdemo.extensions.dp2px
+import com.example.interestingdemo.extensions.systemMediumAnimTime
+import com.example.interestingdemo.extensions.systemShortAnimTime
 import com.example.interestingdemo.extensions.toast
 import kotlinx.android.synthetic.main.dialog_sure_btn.view.*
 import kotlinx.android.synthetic.main.fragment_calculate.*
@@ -19,10 +21,15 @@ import kotlinx.android.synthetic.main.item_calculate_problem.view.*
 
 class CalculateFragment : Fragment() {
     private val countNumber = MutableLiveData<Int>()
+    private val kindArray = ArrayList<Int>()
+    private var maxNumber = 99
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        countNumber.value = 0
+        kindArray.add(0)
+        kindArray.add(1)
+        kindArray.add(2)
+        kindArray.add(3)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -33,7 +40,10 @@ class CalculateFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+//先初始化一次
+        reCreateProblem()
 
+        //监听分数
         countNumber.observe(viewLifecycleOwner, Observer {
             getCount.text = countNumber.value.toString()
             if (getCount.text.toString().toInt() >= 60){
@@ -53,12 +63,101 @@ class CalculateFragment : Fragment() {
             }
         }
 
+        setting.setOnClickListener {
+            showSettingPanel()
+        }
+        overlay.setOnClickListener {
+            hideSettingPanel()
+        }
+
+        reCreate.setOnClickListener {
+            val dialog = LayoutInflater.from(context).inflate(R.layout.dialog_sure_btn, null, false)
+            val alert = AlertDialog.Builder(context).setView(dialog).create()
+            alert.setCancelable(true)
+            dialog.sureTitle.text = "重置确认"
+            dialog.sureMessage.text = "正在重置题目，重置后会情况现有的题目和分数，请确定。"
+            dialog.sureBtn.setOnClickListener {
+                reCreateProblem()
+                alert.dismiss()
+            }
+            dialog.cancelBtn.setOnClickListener {
+                alert.dismiss()
+            }
+            alert.show()
+        }
+
+        changeBtn.setOnClickListener {
+            maxNumber = when {
+                oneNumber.isChecked -> {
+                    9
+                }
+                twoNumber.isChecked -> {
+                    99
+                }
+                threeNumber.isChecked -> {
+                    999
+                }
+                else -> {
+                    99
+                }
+            }
+            if (mathPlus.isChecked){
+                if (!kindArray.contains(0)){
+                    kindArray.add(0)
+                }
+            }else{
+                if (kindArray.contains(0)){
+                    kindArray.remove(0)
+                }
+            }
+            if (mathMinus.isChecked){
+                if (!kindArray.contains(1)){
+                    kindArray.add(1)
+                }
+            }else{
+                if (kindArray.contains(1)){
+                    kindArray.remove(1)
+                }
+            }
+            if (mathMultiply.isChecked){
+                if (!kindArray.contains(2)){
+                    kindArray.add(2)
+                }
+            }else{
+                if (kindArray.contains(2)){
+                    kindArray.remove(2)
+                }
+            }
+            if (mathDivide.isChecked){
+                if (!kindArray.contains(3)){
+                    kindArray.add(3)
+                }
+            }else{
+                if (kindArray.contains(3)){
+                    kindArray.remove(3)
+                }
+            }
+
+            hideSettingPanel()
+        }
+
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,object : OnBackPressedCallback(true){
             override fun handleOnBackPressed() {
                 if ((countNumber.value ?:0) < 60){
                     toast("未达到及格分，无法退出")
                 } else {
-                    findNavController().popBackStack()
+                    val dialog = LayoutInflater.from(context).inflate(R.layout.dialog_sure_btn, null, false)
+                    val alert = AlertDialog.Builder(context).setView(dialog).create()
+                    alert.setCancelable(true)
+                    dialog.sureTitle.text = "退出确认"
+                    dialog.sureMessage.text = "确定要退出吗？"
+                    dialog.sureBtn.setOnClickListener {
+                        findNavController().popBackStack()
+                    }
+                    dialog.cancelBtn.setOnClickListener {
+                        alert.dismiss()
+                    }
+                    alert.show()
                 }
             }
         })
@@ -70,64 +169,107 @@ class CalculateFragment : Fragment() {
                 dialog.sureTitle.text = "恭喜你"
                 dialog.sureMessage.text = "你已经达到了及格分数，现在可以退出学习工具了。"
                 dialog.sureBtn.setOnClickListener {
-                    finish()
                     alert.dismiss()
                 }
                 dialog.cancelBtn.visibility = View.GONE
                 alert.show()
+            } else {
+                toast("请认真做题，至少达到及格分才可以退出。")
             }
         }
 
     }
 
+    private fun reCreateProblem(){
+        countNumber.value = 0
+        maxNumber = 99
+        calculateContainer.removeAllViews()
+        kindArray.clear()
+        kindArray.add(0)
+        kindArray.add(1)
+        kindArray.add(2)
+        kindArray.add(3)
+        twoNumber.isChecked = true
+        mathPlus.isChecked = true
+        mathMinus.isChecked = true
+        mathMultiply.isChecked = true
+        mathDivide.isChecked = true
+    }
+
     private fun createProblem(){
-        val problem = LayoutInflater.from(context).inflate(R.layout.item_calculate_problem,null,false)
-        var checkAnswer = 0
-        when((0..3).random()){
-            0 -> {
-                val firstRan = (0..999).random()
-                val secondRan = (0..999).random()
-                problem.firstNumber.text = "$firstRan"
-                problem.secondNumber.text = "$secondRan"
-                problem.operator.setBackgroundResource(R.drawable.ic_math_jia)
-                checkAnswer = firstRan + secondRan
+        if (kindArray.isNotEmpty()){
+            val problem = LayoutInflater.from(context).inflate(R.layout.item_calculate_problem,null,false)
+            var checkAnswer = 0
+            when(kindArray.random()){
+                0 -> {
+                    val firstRan = (0..maxNumber).random()
+                    val secondRan = (0..maxNumber).random()
+                    problem.firstNumber.text = "$firstRan"
+                    problem.secondNumber.text = "$secondRan"
+                    problem.operator.setBackgroundResource(R.drawable.ic_math_jia)
+                    checkAnswer = firstRan + secondRan
+                }
+                1 -> {
+                    val firstRan = (0.. 2*maxNumber).random()
+                    val secondRan = (0..firstRan).random()
+                    problem.firstNumber.text = "$firstRan"
+                    problem.secondNumber.text = "$secondRan"
+                    problem.operator.setBackgroundResource(R.drawable.ic_math_jian)
+                    checkAnswer = firstRan - secondRan
+                }
+                2 -> {
+                    val firstRan = (0..maxNumber).random()
+                    val secondRan = (0..maxNumber).random()
+                    problem.firstNumber.text = "$firstRan"
+                    problem.secondNumber.text = "$secondRan"
+                    problem.operator.setBackgroundResource(R.drawable.ic_math_cheng)
+                    checkAnswer = firstRan * secondRan
+                }
+                3 -> {
+                    val firstRan = (0..maxNumber).random()
+                    val secondRan = (1..(if (firstRan == maxNumber) 1 else maxNumber-firstRan)).random()
+                    problem.firstNumber.text = "${firstRan * secondRan}"
+                    problem.secondNumber.text = "$secondRan"
+                    problem.operator.setBackgroundResource(R.drawable.ic_math_chu)
+                    checkAnswer = firstRan
+                }
             }
-            1 -> {
-                val firstRan = (0..999).random()
-                val secondRan = (0..firstRan).random()
-                problem.firstNumber.text = "$firstRan"
-                problem.secondNumber.text = "$secondRan"
-                problem.operator.setBackgroundResource(R.drawable.ic_math_jian)
-                checkAnswer = firstRan - secondRan
+            problem.approval.setOnClickListener {
+                if (problem.answerInput.text.toString() == checkAnswer.toString()){
+                    problem.approval.setBackgroundResource(R.drawable.ic_math_right)
+                    countNumber.value = countNumber.value?.plus(1)
+                    problem.approval.isClickable = false
+                    problem.approval.isFocusable = false
+                } else {
+                    problem.approval.setBackgroundResource(R.drawable.ic_math_false_black)
+                }
             }
-            2 -> {
-                val firstRan = (0..99).random()
-                val secondRan = (0..(100-firstRan)).random()
-                problem.firstNumber.text = "$firstRan"
-                problem.secondNumber.text = "$secondRan"
-                problem.operator.setBackgroundResource(R.drawable.ic_math_cheng)
-                checkAnswer = firstRan * secondRan
-            }
-            3 -> {
-                val firstRan = (0..99).random()
-                val secondRan = (1..(100-firstRan)).random()
-                problem.firstNumber.text = "${firstRan * secondRan}"
-                problem.secondNumber.text = "$firstRan"
-                problem.operator.setBackgroundResource(R.drawable.ic_math_chu)
-                checkAnswer = if (firstRan == 0) 0 else secondRan
-            }
+            calculateContainer.addView(problem)
+        } else {
+            toast("请在设置中选择加、减、乘、除中的至少一项！")
         }
-        problem.approval.setOnClickListener {
-            if (problem.answerInput.text.toString() == checkAnswer.toString()){
-                problem.approval.setBackgroundResource(R.drawable.ic_math_right)
-                countNumber.value = countNumber.value?.plus(1)
-                problem.approval.isClickable = false
-                problem.approval.isFocusable = false
-            } else {
-                problem.approval.setBackgroundResource(R.drawable.ic_math_false_black)
-            }
-        }
-        calculateContainer.addView(problem)
+
+    }
+
+    private fun showSettingPanel(){
+        if (overlay.visibility == View.VISIBLE) return
+        overlay.visibility = View.VISIBLE
+        settingPanel.animate()
+                .withStartAction { settingPanel.visibility = View.VISIBLE }
+                .translationY(0f)
+                .alpha(1f)
+                .setDuration(resources.systemMediumAnimTime)
+                .start()
+    }
+
+    private fun hideSettingPanel(){
+        overlay.visibility = View.GONE
+        settingPanel.animate()
+                .translationY(resources.dp2px(100f).toFloat())
+                .alpha(0f)
+                .setDuration(resources.systemMediumAnimTime)
+                .withStartAction { settingPanel.visibility = View.GONE }
+                .start()
     }
 
 }
--- app/src/main/java/com/example/interestingdemo/extensions/Resources.kt
+++ app/src/main/java/com/example/interestingdemo/extensions/Resources.kt
@@ -14,3 +14,28 @@ fun Resources.dp2Px(dp : Float) : Int{
 fun Resources.px2dp(px : Int) : Float{
     return px / displayMetrics.density
 }
+
+/**
+ * 系统动画时间：短
+ */
+val Resources.systemShortAnimTime : Long
+    get() = getInteger(android.R.integer.config_shortAnimTime).toLong()
+
+/**
+ * 系统动画时间：中等
+ */
+val Resources.systemMediumAnimTime: Long
+    get() = getInteger(android.R.integer.config_mediumAnimTime).toLong()
+
+/**
+ * 系统动画时间：长
+ */
+val Resources.systemLongAnimTime: Long
+    get() = getInteger(android.R.integer.config_longAnimTime).toLong()
+
+/**
+ * dp转像素
+ */
+fun Resources.dp2px(dp: Float): Int {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, displayMetrics).roundToInt()
+}
